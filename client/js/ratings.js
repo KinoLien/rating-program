476f8762f6fa6943bc5aa842fc379e6adc2651cd
KinoLien <miecowbai@gmail.com>
2018-01-07 20:52:00 +0800

    Remove unnecessary comments.

--- client/js/ratings.js
+++ client/js/ratings.js
@@ -1,38 +1,6 @@
 
 var socketInstance = io.connect('?role=rating&idx=' + window.rtIdx);
 
-// socketInstance.on('connect', function(msg) {
-//     console.log("Browser Connected. ");
-// });
-// socketInstance.on('pass_token', function(data) {
-//     <% if (typeof isDev == "boolean" && isDev) { %>
-//     // for test
-//     $("#qrcodeText").text(data.token);
-//     <% } %>
-//     qrcodelib.toCanvas($('#qrcode')[0], data.token, function(error) {
-//         if (error) console.error(error)
-//         console.log('QR code generated!');
-//     });
-// });
-// socketInstance.on('pass_compressed_image', function(data) {
-//     var image = '<li>' + '<input type="checkbox" id="pr' +
-//         frame.items.length + '" /><label for="pr' + frame.items.length + '" class="img-label" id="li' + frame.items.length + '"><img id="item-' +
-//         frame.items.length + '" src="data:' + data.type + ';base64,' +
-//         data.base64 + '" path="' + data.filepath + '"/></label>' + '</li>';
-//     frame.add(image);
-//     frame.activate(frame.items.length - 1);
-// });
-// socketInstance.on('connected_with_device', function(data) {
-//     console.log('Connected with device!');
-//     $('#qrcode-modal').modal('hide');
-//     $('body').css('overflow', 'scroll');
-//     $('#connected-modal').modal('show');
-
-//     $('#connected-alert').show();
-
-//     app.$data.loading = false;
-// });
-
 var app = new Vue({
     el: '#input-view',
 
