f11828c420ce2d0bf0c34e796e03daf5b0edcf69
KinoLien <miecowbai@gmail.com>
2018-01-09 10:33:43 +0800

    Revise score view.

--- client/js/scoreview.js
+++ client/js/scoreview.js
@@ -3,9 +3,31 @@ var socketInstance = io.connect('?role=scoreview');
 
 var app = new Vue({
 	el: '#score-view',
-	data:{},
+	data:{
+		remoteData: {}
+	},
 	methods: {},
-	computed: {},
+	computed: {
+		onlyScore: function(){
+			return !this.remoteData.name && this.remoteData.score;
+		},
+		multiScores: function(){
+			return this.remoteData.scores && this.remoteData.scores.length;
+		},
+		scoreWithName: function(){
+			return this.remoteData.name && this.remoteData.score;
+		}
+	},
 	watch: {},
 	mounted: function(){}
 });
+
+// =====================================
+// Socket Handlers =====================
+// =====================================
+['show_score_from_console', 'show_split_score_from_console', 'show_score_with_part_from_console']
+	.forEach(function(evName){
+		socketInstance.on(evName, function(data){
+			app.$data.remoteData = data;
+		});
+	});
